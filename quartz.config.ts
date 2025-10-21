cdded4b708cf6fd5a4bf306b943eb293ceb88d4a
yoloyolo8 <hashyolo123@gmail.com>
2025-10-21 12:54:50 +0800

    Rename vercal.json to vercel.json and update config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "",
+    pageTitle: "Yolonotes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
